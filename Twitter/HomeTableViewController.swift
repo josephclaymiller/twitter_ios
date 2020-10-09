9c4f71d0c2726fb47533aa9a59bc27e2f82a1bb0
Joseph Miller <josephclaymiller@gmail.com>
2020-10-08 17:41:34 -0700

    tweet cell

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -32,12 +32,20 @@ class HomeTableViewController: UITableViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! tweetCell
+        cell.userNameLabel.text = "John Doe"
+        cell.tweetContent.text = "Hello world!"
+        cell.profileImageView.image = #imageLiteral(resourceName: "profile-Icon")
+        return cell
     }
 
     /*
---
+++ Twitter/tweetCell.swift
@@ -0,0 +1,28 @@
+//
+//  tweetCell.swift
+//  Twitter
+//
+//  Created by Joseph Miller on 10/8/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
