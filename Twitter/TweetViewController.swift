e304d66170cb4ded2ab7c3e70bd6aa514a6827c5
Joseph Miller <josephclaymiller@gmail.com>
2020-10-15 02:32:58 -0700

    Compose tweet UI View Controller

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Joseph Miller on 10/15/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: UIBarButtonItem) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
