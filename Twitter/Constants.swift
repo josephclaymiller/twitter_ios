7148740857add6fdc2e04089d0ad2c6a55477bab
Joseph Miller <josephclaymiller@gmail.com>
2020-10-16 11:16:48 -0700

    constants

---
+++ Twitter/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Twitter
+//
+//  Created by Joseph Miller on 10/16/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "LtlxIoQpBvHcqjpSMIA9Gs2E9wCJbr7xkx9EpSdBYoNedaZUgh"
--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "uFTmFW66AAMEUwx3rZlZDMSCf", consumerSecret: "LtlxIoQpBvHcqjpSMIA9Gs2E9wCJbr7xkx9EpSdBYoNedaZUgh")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "uFTmFW66AAMEUwx3rZlZDMSCf", consumerSecret: apiKey)
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
