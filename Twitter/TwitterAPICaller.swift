fec2c3453746bf61fc2c31e76fa97c61eb378831
Joseph Miller <josephclaymiller@gmail.com>
2020-10-16 11:53:33 -0700

    consumerKey and consumerSecret in Constants

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "uFTmFW66AAMEUwx3rZlZDMSCf", consumerSecret: apiKey)
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: consumerKey, consumerSecret: consumerSecret)
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
